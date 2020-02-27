2b97472cd0201d98e6b88595cd0f9a81f7e03c4c
hcgw <hc.wu@mail.utoronto.ca>
2020-02-26 20:43:56 -0500

    started somewhat of an index page so people can get to their respective pages they need to make, included react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div className={"App"}>
+                    <Navigation/>
+                    <Switch>
+                        <Route path={"/"} exact strict component={Home} />
+                        <Route path={"/catalogue"} exact strict component={Catalogue} />
+                        <Route path={"/about"} exact strict component={About} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
+
+class Navigation extends React.Component {
+    render() {
+        return (
+            <nav>
+                <ul className={"navigation-bar"}>
+                    <li><Link to={"/"}>Home</Link></li>
+                    <li><Link to={"/catalogue"}>Browse</Link></li>
+                    <li><Link to={"/about"}>About</Link></li>
+                    <li><button onClick={login}>Sign-in</button></li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type={"text"} id={"search"} name={"search"} value={"Item/Service"}/>
+                    <input type={"submit"} value={"Search"}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+class Catalogue extends React.Component {
+    render() {
+        return (
+            <div>
+                Catalogue page
+            </div>
+        )
+    }
+}
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                About page
+            </div>
+        )
+    }
+}
+
+function login() {
+    return 0;
+}
+
 
 export default App;
