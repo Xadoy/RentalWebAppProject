3d423213629d6cfe21ee0e11b9fd746715a76f64
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2020-02-28 00:41:31 -0500

    implement click for admin sidebar

--- src/react-components/Admin/index.js
+++ src/react-components/Admin/index.js
@@ -1,38 +1,55 @@
 import React from "react";
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 import "./styles.css";
 
 /* Component for the Home page */
 class Sidebar extends React.Component {
-
   render() {
-    const { options, selected } = this.props;
+    const { options, selected, onClickHandler } = this.props;
     return (
-      <div className="sidebar">
-        <List disablePadding dense>
-          {options.map((option) => (
-          <ListItem button>
+      <List disablePadding dense>
+        {options.map((option, index) => (
+          <ListItem
+            button
+            selected={selected === index}
+            onClick={() => onClickHandler(index)}
+          >
             <ListItemText>{option}</ListItemText>
           </ListItem>
-          ))}
-        </List>
-      </div>
+        ))}
+      </List>
     );
   }
 }
 
 class Admin extends React.Component {
   state = {
-    options: ['Manage', 'Listings', 'User Management', 'Requests'],
+    options: ["Manage", "Listings", "User Management", "Requests"],
     selected: 0
-  }
+  };
+
+  handleListItemClick = index => {
+    this.setState({
+      selected: index
+    });
+  };
 
   render() {
     return (
-      <Sidebar options={this.state.options} selected = {this.state.selected}/>
+      <div>
+        <div className="sidebar">
+          <Sidebar
+            options={this.state.options}
+            selected={this.state.selected}
+            onClickHandler={this.handleListItemClick}
+          />
+        </div>
+
+        <div className="admin_panel">test</div>
+      </div>
     );
   }
 }
