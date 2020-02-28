d07244a82adc6ce287f73137a53d4111c8891ba2
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2020-02-28 01:25:36 -0500

    change app.js to not load NavBar, if page is admin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Item from "./components/ItemPage";
 import SearchResults from "./components/SearchResults";
 import About from "./components/AboutPage";
 import Catalogue from "./components/CataloguePage";
 import Home from "./components/HomePage";
 import NavBar from "./components/NavigationBar";
-import Admin from './react-components/Admin';
+import Admin from "./react-components/Admin";
 
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 /* ###################################################################################################################
 
 Everyone should take whatever component/classes relevant to them and create a new .js file for them in the components
@@ -25,26 +28,25 @@ Admin page                                      - unfinished
 #################################################################################################################### */
 
 class App extends React.Component {
+  // I'd like to keep all the link routing here
+  render() {
+    return (
+      <Router>
+        <div className={"App"}>
+          {history.location.pathname !== "/admin" && <NavBar />}
 
-    // I'd like to keep all the link routing here
-
-    render() {
-        return (
-            <Router>
-                <div className={"App"}>
-                    <NavBar/>
-                    <Switch>
-                        <Route path={"/"} exact strict component={Home} />
-                        <Route path={"/catalogue"} exact strict component={Catalogue} />
-                        <Route path={"/about"} exact strict component={About} />
-                        <Route path={"/item/:item_id"} exact strict component={Item} />
-                        <Route path={"/search"} component={SearchResults} />
-                        <Route path={"/admin"} exact strict render={() => (<Admin />)} />
-                    </Switch>
-                </div>
-            </Router>
-        );
-    }
+          <Switch>
+            <Route path={"/"} exact strict component={Home} />
+            <Route path={"/catalogue"} exact strict component={Catalogue} />
+            <Route path={"/about"} exact strict component={About} />
+            <Route path={"/item/:item_id"} exact strict component={Item} />
+            <Route path={"/search"} component={SearchResults} />
+            <Route path={"/admin"} exact strict render={() => <Admin />} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
