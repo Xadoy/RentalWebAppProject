b0ed9ad07ff138c09a45c55a731ec81e9dea5c29
hcgw <hc.wu@mail.utoronto.ca>
2020-02-28 00:02:06 -0500

    picture display proto

--- src/components/itm_pg_components/ItemPictureDisplay.js
+++ src/components/itm_pg_components/ItemPictureDisplay.js
@@ -1,10 +1,33 @@
 import React from "react";
+import apple from './itm_imgs/apple.jpg';
+import apple2 from './itm_imgs/apple2.jpg'
 
 class Pics extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curr_img: ''
+        }
+    }
+    componentDidMount() {
+        this.setState({curr_img: apple})
+    }
     render() {
         return (
-            <div>
-                Images
+            <div className={'display-container'}>
+                <u1 className={"image-choice"}>
+                    <li>
+                        <div className={"img-button"}>
+                            <img className={'icon-display'} src={apple} alt={"apple"}/>
+                            <button className={'behind-img'} onClick={() => this.setState({curr_img: apple})}/>
+                        </div>
+                        <div className={"img-button"}>
+                            <img className={'icon-display'} src={apple2} alt={"apple"}/>
+                            <button className={'behind-img'} onClick={() => this.setState({curr_img: apple2})}/>
+                        </div>
+                    </li>
+                </u1>
+                <img className={'large-img-display'} src={this.state.curr_img} alt={"item_image"}/>
             </div>
         );
     }
