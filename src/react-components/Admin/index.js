e5cc44345e76e19cf4688b721ba3e8641de8ebc8
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2020-03-03 20:19:48 -0500

    implement admin username and password management

--- src/react-components/Admin/index.js
+++ src/react-components/Admin/index.js
@@ -6,6 +6,10 @@ import {
   withStyles,
   Grid,
   Paper,
+  Select,
+  FormControl,
+  TextField,
+  Button,
   makeStyles
 } from "@material-ui/core";
 
@@ -35,7 +39,7 @@ const StyledListItem = withStyles({
   },
   selected: {
     backgroundColor: "red",
-    color: "red",
+    color: "red"
   }
 })(ListItem);
 
@@ -59,9 +63,85 @@ function Sidebar({ options, selected, onClickHandler }) {
   );
 }
 
+class AdminProfileForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: props.username, password: props.password };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.username, this.state.password);
+    // this.handleSubmit submitHandler()
+  }
+
+  render() {
+    return (
+      <form>
+        {/* <InputLabel>Age</InputLabel>
+          Pick your favorite flavor:
+          <Select
+            name="test"
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </Select> */}
+
+        <TextField
+          // id="filled-name"
+          label="Name"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleChange}
+          // variant="filled"
+        />
+        <TextField
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          variant="filled"
+        />
+        <Button
+          type="submit"
+          // fullWidth
+          // variant="contained"
+          // color="primary"
+          // className={classes.submit}
+          onClick={this.handleSubmit}
+        >
+          Save
+        </Button>
+      </form>
+    );
+  }
+}
+
 class ManageView extends React.Component {
   render() {
-    return <div>ManageView</div>;
+    return (
+      <>
+        <div>ManageView</div>
+        <AdminProfileForm
+          onSubmit={this.props.onChange}
+          username={this.props.username}
+          password={this.props.password}
+        />
+      </>
+    );
   }
 }
 
@@ -113,16 +193,29 @@ class RequestsView extends React.Component {
 }
 
 class AdminPanel extends React.Component {
+  state = {
+    username: "admin",
+    password: "password"
+  };
+  handleSubmit(username, password) {
+    this.setState({ username: username, password: password });
+  }
   render() {
-    const {view} = this.props;
+    const { view } = this.props;
     return (
       <>
-        {view === 'Manage' && <ManageView />}
-        {view === 'Listings' && <ListingsView />}
-        {view === 'User Management' && <UserManagementView />}
-        {view === 'Requests' && <RequestsView />}
+        {view === "Manage" && (
+          <ManageView
+            username={this.state.username}
+            password={this.state.password}
+            onChange={this.handleSubmit.bind(this)}
+          />
+        )}
+        {view === "Listings" && <ListingsView />}
+        {view === "User Management" && <UserManagementView />}
+        {view === "Requests" && <RequestsView />}
       </>
-      );
+    );
   }
 }
 
