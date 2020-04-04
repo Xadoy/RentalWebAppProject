b12e068486836db1495f64886d2509d4701f5913
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2020-04-04 09:54:48 -0400

    remove redundant codes in CataloguePage

--- client/src/components/CataloguePage.js
+++ client/src/components/CataloguePage.js
@@ -1,20 +1,12 @@
 import React from "react";
-import SearchBar from "./SearchBar";
 import Pics from "./itm_pg_components/CatalogueItemList";
-import Desc from "./itm_pg_components/ItemDescriptionPanel";
-import Comments from "./itm_pg_components/ItemCommentPanel";
-import Order from "./itm_pg_components/ItemOrderPanel";
-//import { Link } from "react-router-dom";
 
 class Catalogue extends React.Component {
     render() {
         return (
             <div>
-                <SearchBar/>
                 Catalogue page
-                Current item: {this.props.match.params.item_id}
                 <Pics/>
-
             </div>
         )
     }
