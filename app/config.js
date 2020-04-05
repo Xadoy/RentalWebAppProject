b32d0efe71bd7a790d984e2414a123287215b8fd
Yizhi Xu <yizhi.xu@mail.utoronto.ca>
2020-04-04 22:53:30 -0400

    remove exposed secrets

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoURI: process.env.MONGODB_URI || "mongodb://localhost:27017/Team45API",
   adminName: process.env.ADMIN_NAME || "admin",
   adminPassword: process.env.ADMIN_PASSWORD || "admin",
-  cloud_name: process.env.CLOUD_NAME || "dhjsvs4sg",
-  api_key: process.env.API_KEY || "722625429769223",
-  api_secret: process.env.API_SECRET || "URv1thaLiscmXyFC-N--qnEHIrY",
+  cloud_name: process.env.CLOUD_NAME || "",
+  api_key: process.env.API_KEY || "",
+  api_secret: process.env.API_SECRET || "",
 };
